1d188920a7d12734b481f3e27fb6f1bd5511fbab
MuhamadYousuf <muhammadyousuf77998bp@gmail.com>
2024-12-01 22:58:02 -0800

    lab-08

--- my-react-lab/src/App.js
+++ my-react-lab/src/App.js
@@ -1,19 +1,28 @@
-import './App.css';
-import Greeting from './components/greeting';
-import Content from './components/content';
-import Header from './components/header';
-import Footer from './components/footer';
-
+import React from 'react';
+import ProfileCard from './components/ProfileCard';
 
 function App() {
+  const user = {
+    name: 'Yousuf',
+    age: 19,
+    occupation: 'BSCS-Student',
+    location: '' // You can leave this empty or provide a default value if needed
+  };
+
   return (
-    <div className='App'>
-    <Greeting />
-    <Header />
-    <Content />
-    <Footer />
+    <div className="App">
+      <ProfileCard 
+        name={user.name} 
+        age={user.age} 
+        occupation={user.occupation} 
+        location={user.location}
+        ageLabel="Age"
+        occupationLabel="Occupation"
+        locationLabel={user.location ? "Location" : "Location (Not provided)"}
+      />
     </div>
   );
 }
 
 export default App;
+
